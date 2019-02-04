e45d8f17fcd841ff8a8b9900fbd8290fa3b8d2a9
Blanca Sanchez <blancayllen@gmail.com>
2019-02-03 20:43:43 -0800

    store set up complete

--- frontend/src/components/Root.js
+++ frontend/src/components/Root.js
@@ -8,14 +8,6 @@ class App extends Component {
   componentDidMount() {
     const movieThunk = fetchMovies();
     // store.dispatch(movieThunk);
-    //     axios
-    //       .get("/api/v1/search", {
-    //         params: {
-    //           query: "Harry Potter"
-    //         }
-    //       })
-    //       .then(res => console.log(res.data))
-    //       .catch(err => console.error("Oh No! Could not find Campuses!", err));
   }
 
   render() {
